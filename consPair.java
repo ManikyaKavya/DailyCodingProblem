3ffa2d9003f6ec5398160b5339bb7805d0d268f0
Manikya kavya <69142059+ManikyaKavya@users.noreply.github.com>
2020-09-17 09:33:32 +0530

    Add files via upload

---
+++ consPair.java
@@ -0,0 +1,40 @@
+import java.util.*;
+ class Pair<T> {
+
+    private int a;
+    private int b;
+
+    public Pair(int a, int b) {
+        this.a=a;
+        this.b=b;
+	}
+
+    public void getCarValue() {
+        System.out.println("car(cons): "+a);
+    }
+    public void getCdrValue() {
+        System.out.println("cdr(cons): "+b);
+    }
+
+
+}
+public class consPair<T> {
+    
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        Pair<Integer> cons = new Pair<Integer>(a,b);
+        car(cons);
+        cdr(cons);
+        
+    }
+
+    private static void car(Pair<Integer> cons) {
+        cons.getCarValue();
+    }
+    private static void cdr(Pair<Integer> cons) {
+        cons.getCdrValue();
+    }
+}
